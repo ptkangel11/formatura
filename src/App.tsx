58d53727e6a04149538033a1212dd1a5f55f9451
ptkangel11 <88540543+ptkangel11@users.noreply.github.com>
2025-06-03 13:06:06 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ConfirmarPresenca from "./pages/ConfirmarPresenca";
 import NotFound from "./pages/NotFound";
@@ -17,8 +17,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/confirmar-presenca" element={<ConfirmarPresenca />} />
+          {/* Use /* for nested routes under the hash, e.g., #/ */} 
+          <Route path="/*" element={<Index />} /> 
+          <Route path="/confirmar-presenca/*" element={<ConfirmarPresenca />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
